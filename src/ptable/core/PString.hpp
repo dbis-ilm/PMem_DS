462a1e9c31099c276d7a509955f71371a79b540c
Philipp Götze <philipp.goetze@tu-ilmenau.de>
2018-12-19 11:08:29 +0100

    :copyright: Updated Copyright

--- src/ptable/core/PString.hpp
+++ src/ptable/core/PString.hpp
@@ -1,6 +1,23 @@
 /*
-Taken from: https://pmem.io/2017/01/23/cpp-strings.html
-*/
+ * Copyright (C) 2017-2018 DBIS Group - TU Ilmenau, All Rights Reserved.
+ *
+ * This file is part of our NVM-based Data Structure Repository.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* Based on: https://pmem.io/2017/01/23/cpp-strings.html */
 
 #ifndef PString_hpp_
 #define PString_hpp_
--- src/test/PBPTreeTest.cpp
+++ src/test/PBPTreeTest.cpp
@@ -1,20 +1,20 @@
 /*
- * Copyright (C) 2017 DBIS Group - TU Ilmenau, All Rights Reserved.
+ * Copyright (C) 2017-2018 DBIS Group - TU Ilmenau, All Rights Reserved.
  *
- * This file is part of PTable.
+ * This file is part of our NVM-based Data Structure Repository.
  *
- * PTable is free software: you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * PTable is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with PTable.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <algorithm>
